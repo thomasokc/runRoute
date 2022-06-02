03defe34b87d70f27818cd1d91c59e4aefde4a1c
Thomas Morris <thomasmorris682@gmail.com>
2022-06-02 11:09:48 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import * as tt from '@tomtom-international/web-sdk-maps'
 
-// Leaflet Imports
-import {
-  MapContainer,
-  TileLayer,
-  useMap,
-} from 'react-leaflet'
+const App = () => {
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(-97.5164)
+  const [latitude, setLatitude] = useState(35.4676)
 
-function App() {
+  useEffect(() => {
+    
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+      stylesVisibility: {
+        trafficFlow: true,
+        trafficIncidents: true
+      },
+      center: [longitude, latitude],
+      zoom: 14
+    })
+
+    setMap(map)
+
+    const addMarker = () => {
+
+      const element = document.createElement('div')
+      element.className = 'marker'
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+    }
+
+    return () => map.remove()
+  }, [longitude, latitude])
+
   return (
-    <MapContainer center={[35.467560,-97.516426]} zoom={12}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
-  );
+    <div className='app'>
+      <div ref={mapElement} className="map" />
+      <div className='search=-bar'>
+        <h1>Where to?</h1>
+        <input
+          type="text"
+          id="longitude"
+          className='longitude'
+          placeholder='Put in Longitude'
+          onChange={(e) => {setLongitude(e.target.value)}}
+        />
+        <input
+          type="text"
+          id="latitude"
+          className='latitude'
+          placeholder='Put in latitude'
+          onChange={(e) => {setLatitude(e.target.value)}}        
+        />
+      </div>
+    </div>
+  )
 }
 
 export default App;
